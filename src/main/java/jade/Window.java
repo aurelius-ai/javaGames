463a577d5108420a3dceabaa8861e957de02d274
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2022-03-04 13:44:42 -0600

    Exposed variables to the level editor

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -36,18 +36,18 @@ public class Window {
         switch (newScene) {
             case 0:
                 currentScene = new LevelEditorScene();
-                currentScene.init();
-                currentScene.start();
                 break;
             case 1:
                 currentScene = new LevelScene();
-                currentScene.init();
-                currentScene.start();
                 break;
             default:
                 assert false : "Unknown scene '" + newScene + "'";
                 break;
         }
+
+        currentScene.load();
+        currentScene.init();
+        currentScene.start();
     }
 
     public static Window get() {
@@ -153,6 +153,8 @@ public class Window {
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+
+        currentScene.saveExit();
     }
 
     public static int getWidth() {
